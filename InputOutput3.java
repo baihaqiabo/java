a70d1c8e978feaf8e745651f554fb8ee982c8d80
baihaqiabo <iqbal.baihaqi27@gmail.com>
2017-03-19 16:56:45 +0700

    Belajar Java

---
+++ InputOutput3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class InputOutput3 {
+	public static void main (String[] args) {
+		Scanner input = new Scanner (System.in);
+		String nama;
+		System.out.println ("Arman");
+
+		nama = input.nextLine();
+	}
+}
